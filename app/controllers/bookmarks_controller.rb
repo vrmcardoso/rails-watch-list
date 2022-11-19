4f41114d65a4403d33bd638b83646251f5bbf4cb
vrmcardoso <vrmcardoso@gmail.com>
2022-11-19 12:00:59 +0000

    changed new bookmark redirect to the list itself

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -10,7 +10,7 @@ class BookmarksController < ApplicationController
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list_id = @list.id
     if @bookmark.save
-      redirect_to lists_path(@list)
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
